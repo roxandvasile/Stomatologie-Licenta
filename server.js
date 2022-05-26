28346246e6405f90477ee0f6288a79361b0d4f25
RoxanaAndreeaVasile <roxanaandreeavasile@gmail.com>
2022-05-26 19:55:33 +0300

    now

--- server.js
+++ server.js
@@ -3,34 +3,34 @@ let app = express();
 
 app.use(express.static('public'));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/index.html', function(req, res) {
+app.get('/index.html', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/despre.html', function(req, res) {
+app.get('/despre.html', function (req, res) {
     res.sendFile(__dirname + '/despre.html');
 });
 
-app.get('/echipa.html', function(req, res) {
+app.get('/echipa.html', function (req, res) {
     res.sendFile(__dirname + '/echipa.html');
 });
 
-app.get('/preturi.html', function(req, res) {
+app.get('/preturi.html', function (req, res) {
     res.sendFile(__dirname + '/preturi.html');
 });
 
-app.get('/galerie.html', function(req, res) {
+app.get('/galerie.html', function (req, res) {
     res.sendFile(__dirname + '/galerie.html');
 });
 
-app.get('/contact.html', function(req, res) {
-    res.sendFile(__dirname + '/contact.html');
+app.get('/blog.html', function (req, res) {
+    res.sendFile(__dirname + '/blog.html');
 });
 
-app.listen(4000, function() {
+app.listen(4000, function () {
     console.log('Loading app port 4000');
 });
