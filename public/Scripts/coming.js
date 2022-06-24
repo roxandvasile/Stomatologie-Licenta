0ad8520648d7be0550cedfe0e627d2677074f92c
RoxanaAndreeaVasile <roxanaandreeavasile@gmail.com>
2022-06-24 13:19:49 +0300

    new

--- public/Scripts/coming.js
+++ public/Scripts/coming.js
@@ -1,13 +1,13 @@
-var countDownDate = new Date("September 1, 2022 00:00:00").getTime();
+var countDownDate = new Date("July 8, 2022 00:00:00").getTime();
 
 var x = setInterval(function () {
   var now = new Date().getTime();
   var distance = countDownDate - now;
 
-  var days = Math.floor(distance / (2000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (2000 * 60 * 60 * 24)) / (2000 * 60 * 60));
-  var minutes = Math.floor((distance % (2000 * 60 * 60)) / (2000 * 60));
-  var seconds = Math.floor((distance % (2000 * 60)) / 2000);
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   document.getElementById("launch").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
@@ -15,4 +15,4 @@ var x = setInterval(function () {
     clearInterval(x);
     document.getElementById("launch").innerHTML = "EXPIRED";
   }
-}, 2000);
+}, 1000);
