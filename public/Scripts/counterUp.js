fa90630b5be86fd7969baa883855ea602ed429b7
RoxanaAndreeaVasile <roxanaandreeavasile@gmail.com>
2022-06-24 12:14:45 +0300

    new

--- public/Scripts/counterUp.js
+++ public/Scripts/counterUp.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   $(".counter").counterUp({
-    delay: 50,
-    time: 1200,
+    delay: 50,  //intarzierea in milisecunde 
+    time: 1200,  //durata totala a animatiei
   });
 });
