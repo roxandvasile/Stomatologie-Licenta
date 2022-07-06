c39be4fb1e80fe426683a945ce0a33b056a407eb
RoxanaAndreeaVasile <roxanaandreeavasile@gmail.com>
2022-07-06 09:12:25 +0300

    new

--- public/Scripts/search.js
+++ public/Scripts/search.js
@@ -8,10 +8,13 @@ function tableSearch() {
     table = document.getElementById("myTable");
     tr = document.getElementsByTagName("tr");
 
-    //parcurgem toate randurile tabelului si le ascundem pe cele care nu se potrivesc
+    //parcurgem toate randurile tabelului 
+
     for (let i = 0; i < tr.length; i++) {
         td = tr[i].getElementsByTagName("td")[1]
 
+        //daca serviciul se regaseste in tabel, este afisat si vom ascundem serviciile care nu se potrivesc
+        //daca servciul nu este gasit in tabel, nu se afiseaza nimic
         if (td) {
             txtValue = td.textContent || td.innerText;
             if (txtValue.toUpperCase().indexOf(filter) > -1) {
